1c649b57c1f9e6b0921f63f452869423b11e8cd4
SaMbeet <sambeets@gmail.com>
2019-02-06 13:53:17 +0200

    Working

--- Swedbank_Problem.py
+++ Swedbank_Problem.py
@@ -2,34 +2,129 @@
 #
 # Imports
 #
-# wl_abstract.py: AbstractModel version of warehouse location determination problem
+
 from pyomo.environ import *
 
-model = AbstractModel()
-model.i = Set() # Set of time
-model.j = Set() # Set of day
-model.k = Set() # Set of tasks
-model.v = Set() # Set of employees
+###################################
+# Model Begins
+###################################
+
+
+model = AbstractModel("Swedbank Scheduling Tartu Uni 2019")
+
+model.Ntime = Param() # number of time slots 
+model.Nday = Param() # number of days
+model.Ntask = Param() # number of Tasks 
+model.n = Param() # number of employees
+
+model.S = RangeSet(1,model.Ntime) # Set of time
+model.D = RangeSet(1,model.Nday) # Set of day
+model.T = RangeSet(1,model.Ntask) # Set of tasks 
+model.V = RangeSet(1,model.n) # Set of employees
 
 # Parameters
-model.c = Param(model.k, within= PositiveReals)
-model.theta1 = Param()
+
+model.c = Param(model.T, within= PositiveReals) # duration of tasks of type k 
+model.a = Param(model.S, model.D, within= PositiveReals) # delay proirities
+model.w = Param(model.S, model.D, model.T, within= PositiveReals) # number of tasks (type k) received at time (i,j)
+model.theta0 = Param(within= PositiveReals) # preparation time 
+model.theta1 = Param(within= PositiveReals) # starting time of batch 1
+model.theta2 = Param(within= PositiveReals) # starting time of batch 2
+model.theta3 = Param(within= PositiveReals) # the slot index of the starting time of the lunch break
+model.theta4 = Param(within= PositiveReals) # the slot index of the ending time of the lunch break
+model.theta5 = Param(within= PositiveReals) # the duration of the lunchtime
+model.Maxtime = Param(within=PositiveReals)
+model.Mintime = Param(within=PositiveReals)
 
 # Variables
-model.alpha = Var(model.i, model.j, model.v, within=Binary) # parameter alpha
-model.beta = Var(model.i, model.j, model.v, within=Binary) # parameter beta
-model.tao = Var(model.i, model.j, model.v, within=Binary) # parameter beta
-model.delta = Var(model.i, model.j, model.v, within=NonNegativeIntegers, default=0.0) # variable
+model.alpha = Var(model.S, model.D, model.V, within=Binary) # variable alpha (whether i,j is the starting time of employes v)
+model.beta = Var(model.S, model.D, model.V, within=Binary) # variable beta (whether i,j is the ending time of employees v)
+model.tau = Var(model.S, model.D, model.V, within=Binary) # variable tau (availability employee working at time i,j )
+model.delta = Var(model.S, model.D, within=NonNegativeReals) # variable (amount of delay at time i,j ) 
 
 
+# =============================================================================
+# # Constraints
+# 
+
+def const11_rule(model, v):
+    return sum(model.tau[i,j,v] for i in model.S for j in model.D ) <= model.Maxtime - model.theta1
+model.const11 = Constraint(model.V, rule=const11_rule)
 
 
-# Constraints
+def const12_rule(model, v):
+    return sum(model.tau[i,j,v] for i in model.S for j in model.D ) >= model.Mintime - model.theta1
+model.const12 = Constraint(model.V, rule=const12_rule)
 
-def const1_rule(model,i,j):
-    return sum(sum(model.tao[i,j,v] for i in model.i) for j in model.j ) == 160- model.theta1
-model.const1_cust = Constraint(model.i, model.j, model.v, rule=one_per_cust_rule)
+# 
+
+def const2_rule(model,j,v):
+    return sum(model.alpha[i,j,v] for i in model.S) == 1
+model.const2 = Constraint(model.D, model.V, rule = const2_rule)
+# 
+
+def const3_rule(model,j,v):
+    return sum(model.beta[i,j,v] for i in model.S) == 1
+model.const3 = Constraint(model.D, model.V, rule = const3_rule)
+
+# 
+def const4_rule(model,i,j,v):
+    if i != model.theta1 and i != model.theta2:
+        return model.alpha[i,j,v] == 0
+    else:
+        return model.alpha[i,j,v] >= 0
+model.const4 = Constraint(model.S, model.D, model.V,  rule = const4_rule)
+# 
+
+def const5_rule(model,i,j,v):
+    val5 = sum(model.alpha[k,j,v] for k in range(1,i+1) )
+    return model.tau[i,j,v] <= val5
+model.const5 = Constraint(model.S, model.D, model.V, rule = const5_rule)
+# 
+
+def const6_rule(model,i,j,v):
+    val6 = 1 - sum(model.beta[k,j,v] for k in range(1,i) )
+    return model.tau[i,j,v] <= val6
+model.const6 = Constraint(model.S, model.D, model.V, rule=const6_rule)
+# 
+
+def const7_rule(model,i,j):
+    return sum( model.c[k] * model.w[i,j,k] for k in model.T ) <= sum( model.tau[i,j,k] for k in model.V ) + model.delta[i,j]
+model.const7 = Constraint(model.S, model.D, rule = const7_rule)
 
 
+'''
+# Lunch time
+def const8_rule(model,j):
+    lhs_sum = 0
+    rhs_sum = 0
+    for i in model.S:
+        if i >= model.theta3 or i <= model.theta4:
+            lhs_sum += sum(model.c[k] * model.w[i,j,k] for k in model.T)
+            rhs_sum += sum( model.tau[i,j,k] for k in model.V)
+        #^ if
+    #^ for
+    return lhs_sum <= rhs_sum + model.delta[i,j]- model.n * model.theta5
+model.const8 = Constraint(model.D, rule = const8_rule)
+'''
+#
 
-# pyomo solve --solver=glpk 02Model_Pyomo.py model2.dat
+def objective_rule(model):
+    return sum(model.a[i,j] * model.delta[i,j] for i in model.S for j in model.D)
+model.objective = Objective(rule=objective_rule, sense=minimize)
+'''
+opt = SolverFactory('cplex')
+instance = model.create_instance("data2.dat") # data.dat contains all data | data2.dat conntains data without w[i,j,k]
+results = opt.solve(instance) # solves and updates instance
+instance.load(results)
+results.write()
+#instance.display()
+
+#model.pprint()
+'''
+
+# 
+# 
+# # pyomo solve --solver=glpk Swedbank_Problem.py data2.dat
+# 
+# =============================================================================
